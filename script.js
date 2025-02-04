2043a36245a79a628793af09d50370489d3b6c25
Leo Drevon <leo.drevon@epitech.eu>
2025-02-04 18:58:30 +0100

    send also final site

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const navMenu = document.querySelector('nav ul');
+
+    if (burgerMenu && navMenu) {
+        burgerMenu.addEventListener('click', function () {
+            navMenu.classList.toggle('visible');
+            console.log('Burger menu clicked, menu visibility:', navMenu.classList.contains('visible') ? 'Visible' : 'Hidden');
+        });
+    }
+
+    // Ajout de la gestion du lien actif
+    const links = document.querySelectorAll('nav a');
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            links.forEach(l => l.classList.remove('active')); // Supprime les classes actives
+            link.classList.add('active'); // Ajoute la classe active
+        });
+    });
+});
